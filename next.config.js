aaa65f6b5d7748335dfe2a51df681b126bda0311
doaashafik <doaa.mouhasseb@yahoo.com>
2023-06-15 23:44:32 +0300

    setup project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'admin.medexsepeti.com',
+                port: '',
+                pathname: '/graph/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn1.medexsepeti.com',
+                port: '',
+                pathname: '/dims/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.medexsepeti.com',
+                port: '',
+                pathname: '/image/**',
+            },
+        ],
+    },
 
+}
 module.exports = nextConfig
